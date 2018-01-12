f5d653783186262b937b6c5be9940d98589b02b6
David Novogrodsky <david.novogrodsky@gmail.com>
2018-01-11 21:17:53 -0600

    Experiments DJN

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,8 +1,30 @@
 /*
-Hello
+Hello, set language in File ->Project Structure
  */
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+
+    // the following line has a value that is too large
+    // int largeInt = 2_344_987_998;
+    int testInt = 2_333_237;
+
+    // width of int = 32 bits
+    // width of float = 32 bits
+    // width of double 64 bits
+
+    // char and boolean
+    char myChar = 'T';
+    char newChar = '3';
+    char testCode = '\u2764';
+
+    System.out.println("Test of unicode " + testCode);
+
+    String myString = "Testing the string";
+
+
+  }
 }
